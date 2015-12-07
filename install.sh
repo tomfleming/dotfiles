b7b8609d882d15d3ecf41a18823f9ccbe05313b3
Tom Fleming <tomdfleming@gmail.com>
2015-12-06 22:12:29 -0800

    support file symlinks only, no dirs; add force option

--- install.sh
+++ install.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 dotfiles_dir=~/dotfiles
-files=".gitignore_global .oh-my-zsh/custom .tmux.conf .vim .vimrc .zshrc"
+backup_dir=~/dotfiles_old
+files=".gitignore_global .oh-my-zsh/custom/aliases.zsh .tmux.conf .vimrc .zshrc"
+backup=0
+
+# Lifted option handling syntax from http://mywiki.wooledge.org/BashFAQ/035
+while :; do
+    case $1 in
+        -f|--force)
+            backup=1
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            break
+    esac
+    shift
+done
 
 for file in $files; do
     dst=~/$file
@@ -9,6 +27,19 @@ for file in $files; do
         echo "Symlinking $dst..."
         ln -s $dotfiles_dir/$file $dst
     else
-        echo "$dst already exists... No symlink created"
+        if [ $backup = 1 ] ; then
+            budst=$backup_dir/$file
+            echo "Attempting to backup $dst to $budst and creating symlink"
+            if [ ! -e $backup_dir ]; then
+                mkdir $backup_dir
+            fi
+            if [ ! -e $backup_dir/.oh-my-zsh/custom ]; then
+                mkdir -p $backup_dir/.oh-my-zsh/custom
+            fi
+            mv $dst $budst
+            ln -s $dotfiles_dir/$file $dst
+        else
+            echo "$dst already exists... No symlink created"
+        fi
     fi
 done
