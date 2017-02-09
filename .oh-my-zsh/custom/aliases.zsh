1d2fe76cbfc896c43a714e09c231d2ef331692a0
Tom Fleming <tomflem@uw.edu>
2017-02-08 22:33:14 -0800

    cleanup alias for docker

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,4 @@
 alias ls="ls -GFlsh"
 alias vim="mvim -v"
 alias vimdiff="mvimdiff -v"
+alias docker-cleanup='docker rm -v $(docker ps -a -q -f status=exited) && docker rmi $(docker images -f "dangling=true" -q)'
